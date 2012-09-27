1544122f187b4f37d655ea4f4e3a759f53171967
Hung En Tsai <ht02135@yahoo.com>
2012-09-27 16:15:07 -0300

    Update webapp-module/main/java/com/hung/controller/LoginController.java
    edit comment

--- webapp-module/main/java/com/hung/controller/LoginController.java
+++ webapp-module/main/java/com/hung/controller/LoginController.java
@@ -23,6 +23,16 @@ public class LoginController {
 	@Autowired
 	private LoginService loginService;
 	
+	/*
+	1>Before invoking the handler method, Spring invokes all the methods that have @ModelAttribute annotation. 
+	It adds the data returned by these methods to a temporary Map object. The data from this Map would be added 
+	to the final Model after the execution of the handler method.
+		
+        2>Then it prepares to invoke the the handler method. To invoke this method, it has to resolve the arguments. 
+        If the method has a parameter with @ModelAttribute, then it would search in the temporary Map object with 
+        the value of @ModelAttribute. If it finds, then the value from the Map is used for the handler method parameter.
+	*/
+	
 	// populate jaxbClientSession for the first time if its null
 	@ModelAttribute("jaxbClientSession")
 	public JaxbClientSession populateClientSession() {
@@ -40,16 +50,6 @@ public class LoginController {
 		}
 		return "login";	// return 'login' tile definition
 	}
-	
-	/*
-		1>Before invoking the handler method, Spring invokes all the methods that have @ModelAttribute annotation. 
-		It adds the data returned by these methods to a temporary Map object. The data from this Map would be added 
-		to the final Model after the execution of the handler method.
-		
-        2>Then it prepares to invoke the the handler method. To invoke this method, it has to resolve the arguments. 
-        If the method has a parameter with @ModelAttribute, then it would search in the temporary Map object with 
-        the value of @ModelAttribute. If it finds, then the value from the Map is used for the handler method parameter.
-	*/
 	
 	@RequestMapping(method=RequestMethod.POST)
 	public String login(@ModelAttribute("jaxbClientSession") JaxbClientSession jaxbClientSession, BindingResult bindingResult, Model model)
