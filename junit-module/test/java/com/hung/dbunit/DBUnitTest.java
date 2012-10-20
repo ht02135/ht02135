3c78a8d2c2a3738bfe1ef6fc9cb30093faa0c301
Hung En Tsai <ht02135@yahoo.com>
2012-10-20 15:29:01 -0400

    dbunit
    dbunit factor out db fixture

--- junit-module/test/java/com/hung/dbunit/DBUnitTest.java
+++ junit-module/test/java/com/hung/dbunit/DBUnitTest.java
@@ -1,7 +1,6 @@
 package com.hung.dbunit;
 
 import java.io.FileInputStream;
-import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -14,26 +13,17 @@ import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.hung.auction.dao.DomainDAO;
-import com.hung.auction.domain.Domain;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:applicationContext-test2.xml"})
 public class DBUnitTest {
 
     private static Logger log = Logger.getLogger(DBUnitTest.class);
-    
-    // this instance will be dependency injected by name
-    @Autowired
-    @Qualifier("domainDAO")
-    private DomainDAO domainDAO;
     
     @Autowired 
     @Qualifier("dataSource")
@@ -62,13 +52,5 @@ public class DBUnitTest {
     public void tearDown() throws Exception {
         dbTester.setTearDownOperation(DatabaseOperation.DELETE_ALL);
         dbTester.onTearDown();
-    }
-    
-    @Test
-    public void testDummy() {
-        List<Domain> domans = domainDAO.findAll();
-        log.info("*********************************************");
-        log.info("domans="+domans);
-        log.info("*********************************************");
     }
 }
