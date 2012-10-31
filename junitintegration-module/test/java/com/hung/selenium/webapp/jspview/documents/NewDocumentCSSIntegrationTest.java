08115ee69b67c15fd957f9f5e54e234bce08e74b
Hung En Tsai <ht02135@yahoo.com>
2012-10-31 18:04:38 -0400

    tested javascript validate via selenium
    tested javascript validate via selenium

---
+++ junitintegration-module/test/java/com/hung/selenium/webapp/jspview/documents/NewDocumentCSSIntegrationTest.java
@@ -0,0 +1,79 @@
+package com.hung.selenium.webapp.jspview.documents;
+
+import junit.framework.Assert;
+
+import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.hung.selenium.css.MyDefaultSelenium;
+import com.hung.selenium.css.MySelenium;
+
+public class NewDocumentCSSIntegrationTest {
+    
+    private static Logger log = Logger.getLogger(NewDocumentCSSIntegrationTest.class);
+    
+    private static MySelenium selenium;
+    
+    @BeforeClass
+    public static void setUpOnce() throws Exception { 
+        gotoNewDocumentCSS();
+    }
+    
+    private static void gotoNewDocumentCSS() throws Exception {
+        selenium = new MyDefaultSelenium("localhost", 4444, "*chrome", "http://127.0.0.1:8081/simple-restfulwebapp-module/auction/");
+        selenium.start();
+        
+        // open
+        selenium.open("http://127.0.0.1:8081/simple-restfulwebapp-module/auction/");
+        
+        // login
+        String jsessionid = selenium.getCookieByName("JSESSIONID");
+        selenium.click("css=a[href='/simple-restfulwebapp-module/auction/login;jsessionid="+jsessionid+"']");
+        selenium.waitForPageToLoad("30000");
+        selenium.type("css=input#domainName", "root");  // id=domainName
+        selenium.type("css=input#loginId", "admin");    // id=loginId
+        selenium.click("css=input[name='login'][type='submit']");      // multiple condition => name=login, type=submit
+        selenium.waitForPageToLoad("30000");
+        
+        // click document manager link
+        selenium.click("css=a[href='/simple-restfulwebapp-module/auction/documents']");
+        selenium.waitForPageToLoad("30000");
+    }
+    
+    // ########## Functional test of domain users page ##################################################
+    
+    @Test
+    public void testNewDocumentForm() {
+        // <input type="text" value="" name="name" id="name">
+        Assert.assertTrue(selenium.isElementPresent("css=form#document input#name"));
+        
+        // <input type="file" id="file" name="file">
+        Assert.assertTrue(selenium.isElementPresent("css=form#document input#file"));
+        
+        // <input type="submit" value="Add Document">
+        Assert.assertTrue(selenium.isElementPresent("css=form#document :submit[type='submit']"));
+    }
+    
+    @Test
+    public void testNewDocumentJavaScript() {
+        selenium.fireEvent("css=form#document :submit[type='submit']", "submit");   // simulate submit event
+        selenium.click("css=form#document :submit[type='submit']"); 
+        
+        boolean isAlert = selenium.isAlertPresent();
+        Assert.assertTrue(isAlert);
+        
+        if (isAlert) {
+            String alert = selenium.getAlert();
+            //log.info("alert=|"+alert+"|");
+            Assert.assertEquals("name : Required Field ", alert);
+        }
+    }
+    
+    @AfterClass
+    public static void tearDownOnce() throws Exception {
+        selenium.close();
+        selenium.stop();
+    }
+}
